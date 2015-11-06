12199ba17f0dd04963dd1fcf7b394c8300a551d1
41407 <jimi.welling@cs.helsinki.fi>
2015-11-06 09:43:42 +0200

    Start input controller

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour
+{
+	public Vector3 mousePosition;
+	public GameObject flightWheel;
+	public Camera cam;
+
+	void Start ()
+	{
+		flightWheel = GameObject.Find ("Flight Wheel");
+		cam = Camera.main;
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			float distanceToGround = 0;
+			Physics.Raycast (cam.ScreenToWorldPoint(Input.mousePosition), cam.transform.forward, out hit);
+			print (hit.point);
+		}
+	}
+}
