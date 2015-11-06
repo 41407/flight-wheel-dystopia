354fb02a8c664460fff7d9bf229c5f1e85a3e62d
41407 <jimi.welling@cs.helsinki.fi>
2015-11-06 10:17:01 +0200

    Add input for throw force Y component

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MouseController : MonoBehaviour
 {
 	public float forceFactor = 50;
+	public float forceY;
 	public Vector3 forceVector;
 	public Vector3 mousePosition;
 	public GameObject flightWheel;
@@ -19,16 +20,27 @@ public class MouseController : MonoBehaviour
 
 	void Update ()
 	{
-		if (Input.GetMouseButton (0)) {
+		if (Input.GetMouseButtonDown (0)) {
 			RaycastHit hit;
 			float distanceToGround = 0;
-			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
 			Physics.Raycast (ray, out hit);
-			print (hit.point);
+			//print (hit.point);
 			forceVector = hit.point - flightWheel.transform.position;
+		} else if (Input.GetMouseButton (0) && forceVector.magnitude > 0) {
+			forceY = GetWorldPositionOnPlane (Input.mousePosition, forceVector).y;
 		} else if (Input.GetMouseButtonUp (0) && forceVector.magnitude > 0) {
-			flightWheelBody.AddForce (forceVector * forceFactor + Vector3.up * forceVector.magnitude * forceFactor);
+			flightWheelBody.AddForce (forceVector * forceFactor + Vector3.up * forceY * forceFactor);
 			print ("Force of " + forceVector.magnitude + " added!");
 		}
+	}
+	
+	public Vector3 GetWorldPositionOnPlane (Vector3 mousePosition, Vector3 position)
+	{
+		Ray ray = cam.ScreenPointToRay (mousePosition);
+		Plane xy = new Plane (Vector3.forward, position);
+		float distance;
+		xy.Raycast (ray, out distance);
+		return ray.GetPoint (distance);
 	}
 }
